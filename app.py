d6e513e6bcb70d003fb014a631f0e0f3a2b4312e
Asmita Mukherjee <85896644+AshM29@users.noreply.github.com>
2024-01-20 19:40:50 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+st.title('Weather Forecast')
+st.write('This is a demo app')
+with st.sidebar:
+  st.write('This is a sidebar')
+col1,col2=st.columns(2)
+with col1:
+   a=st.number_input('Enter a value',value=10)
+with col2:
+   b=st.text_input('Enter text')
+sub=st.button(label='Submit')
+if sub:
+  st.write(a)
+  st.write(b)
+print(a)
+
+  
